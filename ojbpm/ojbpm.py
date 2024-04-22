ac3f529772a3514b64fc2caeeb8d6b0cb9e9cddb
TDV Alinsa <alinsa-github@lupine.org>
2024-04-22 08:59:17 -0700

    add saving and loading of current state

--- ojbpm/ojbpm.py
+++ ojbpm/ojbpm.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 import argparse
 import enum
+import json
 import os
 import sys
 import time
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 from datetime import datetime
 from pathlib import Path
-from typing import Iterable, List, Optional, TypeVar
+from typing import Iterable, List, Literal, Optional, TypeVar
 
 import mido
 from tdvutil import ppretty
@@ -15,14 +16,11 @@ from tdvutil.argparse import CheckFile
 
 MIDI_CLOCKS_PER_BEAT = 24
 
-# Make timestamps match up with the onglog
-os.environ['TZ'] = 'US/Eastern'
-time.tzset()
+# Make timestamps match up with the onglog (unixes only, for some reason)
+if hasattr(time, "tzset"):
+    os.environ['TZ'] = 'US/Eastern'
+    time.tzset()
 
-class PlaybackState(enum.Enum):
-    UNKNOWN = enum.auto()
-    STOPPED = enum.auto()
-    STARTED = enum.auto()
 
 # Eventually we'll want to be able to persist some state (so we can restart
 # this code in the middle of a loop or something and have it still know the
@@ -30,11 +28,55 @@ class PlaybackState(enum.Enum):
 # don't want to persist we should leave out from here.
 @dataclass
 class LoopState:
-    playback_state: PlaybackState = PlaybackState.UNKNOWN
+    playback_state: Literal["UNKNOWN", "STOPPED", "STARTED"] = "UNKNOWN"
     looper_slot: str = "0-0"
-    playback_start_time: Optional[float] = None
-    playback_stop_time: Optional[float] = None
+    playback_start_time: float = 0.0
+    playback_stop_time: float = 0.0
     current_bpm: float = 0
+    updated_t: float = 0
+
+    def to_json(self) -> str:
+        return json.dumps(asdict(self))
+
+    @classmethod
+    def from_json(cls, data) -> 'LoopState':
+        s = json.loads(data)
+        return cls(**s)
+
+
+# FIXME: Should these be part of LoopState?
+def save_state(statefile: Optional[Path], ls: LoopState):
+    if statefile is None:
+        return
+
+    ls.updated_t = time.time()
+    state_json = ls.to_json()
+    tmpfile = statefile.with_suffix(".tmp")
+    tmpfile.write_text(state_json)
+
+    tmpfile.replace(statefile)
+    log(f"SAVED CURRENT STATE (to {statefile})")
+
+
+def init_state(statefile: Optional[Path]) -> LoopState:
+    if statefile is None:
+        return LoopState()
+
+    try:
+        state_json = statefile.read_text()
+        state = LoopState.from_json(state_json)
+
+        # make sure the state is kinda recent (last 15 mins) or just ignore it
+        if time.time() > (state.updated_t + (15 * 60)):
+            log("SAVED STATE IGNORED (too old)")
+            return LoopState()
+
+        # else
+        log(f"SAVED STATE LOADED (from {statefile})")
+        return state
+    except Exception as e:
+        log(f"SAVED STATE NOT AVAILABLE ({e})")
+        return LoopState()
 
 def log(msg: str) -> None:
     now = datetime.now()
@@ -91,9 +133,15 @@ def parse_args() -> argparse.Namespace:
         help="Generate fake midi clock at this bpm for testing purposes",
     )
 
-
+    parser.add_argument(
+        "--state-file", "--statefile",
+        type=Path,
+        default=None,
+        help="Location to persist state during restarts and similar."
+    )
 
     return parser.parse_args()
+
 
 def main() -> int:
     log("Starting up")
@@ -126,7 +174,7 @@ def main() -> int:
 
         now = time.time
 
-    lstate = LoopState()
+    lstate = init_state(args.state_file)
 
     clock_tick_count: int = 0
     start_tick_time: float = 0.0
@@ -138,7 +186,7 @@ def main() -> int:
     for msg in midi:
         if msg.type == "clock":
             fake_time += fake_time_per_tick
-            if lstate.playback_state == PlaybackState.STOPPED:
+            if lstate.playback_state == "STOPPED":
                 continue
             if clock_tick_count == 0:
                 start_tick_time = now()
@@ -146,39 +194,51 @@ def main() -> int:
 
             # Not sure why the +1 is needed here, really
             if clock_tick_count == (MIDI_CLOCKS_PER_BEAT * args.sample_beats) + 1:
-                clock_tick_count = 0
+                # don't count a few ticks so we can make sure we're caught up
+                # from saving or whatever before starting again
+                clock_tick_count = -20
                 elapsed = now() - start_tick_time
                 time_per_beat = elapsed / args.sample_beats
-                lstate.current_bpm = round(60.0 / time_per_beat, 1)
-                log(f"BPM: {lstate.current_bpm:0.1f}")
+                new_bpm = round(60.0 / time_per_beat, 1)
+
+                if lstate.current_bpm != new_bpm:
+                    lstate.current_bpm = new_bpm
+                    log(f"NEW BPM: {lstate.current_bpm:0.1f}")
+                    save_state(args.state_file, lstate)
 
         elif msg.type == "program_change":
             # Changing to a new slot
             lstate.looper_slot = program_to_slot(msg.program)
+            lstate.current_bpm = 0.0
+            clock_tick_count = -20
             log(f"SLOT CHANGE: {lstate.looper_slot}")
+            save_state(args.state_file, lstate)
 
         elif msg.type == "start":
-            if lstate.playback_state == PlaybackState.STARTED:
+            if lstate.playback_state == "STARTED":
                 log("START (ignored)")
             else:
-                lstate.playback_state = PlaybackState.STARTED
-                clock_tick_count = 0
+                lstate.playback_state = "STARTED"
+                clock_tick_count = -20
                 lstate.playback_start_time = now()
                 log("START")
+                save_state(args.state_file, lstate)
 
         elif msg.type == "stop":
-            if lstate.playback_state == PlaybackState.STOPPED:
+            if lstate.playback_state == "STOPPED":
                 log("STOP (ignored)")
             else:
-                lstate.playback_state = PlaybackState.STOPPED
+                lstate.playback_state = "STOPPED"
                 lstate.playback_stop_time = now()
                 log("STOP")
+                save_state(args.state_file, lstate)
 
         elif msg.type == "sysex":
             # Drop the checksum byte
             sysex_str = " ".join([h(x) for x in group(msg.data[:-1], 2)])
             log(f"SYSEX: {sysex_str}")
 
+            # FIXME: should we save the state?
             if lstate.current_bpm > 0:
                 loop_length = ((msg.data[8] & 0x0f) << 4) + (msg.data[9] & 0x0f)
                 loop_length_s = (60.0 / lstate.current_bpm) * 4 * loop_length
@@ -188,6 +248,8 @@ def main() -> int:
 
         else:
             log(f"UNKNOWN: {mido.format_as_string(msg)}")
+
+    return 0
 
 
 if __name__ == "__main__":
