f97160861dd238c30f0a44224df3a9a8a1fe8c4e
TDV Alinsa <alinsa-github@lupine.org>
2024-05-27 18:52:05 -0700

    looper/stemstream: minor fixes and logging cleanup

--- looperstream/looperstream.py
+++ looperstream/looperstream.py
@@ -23,6 +23,11 @@ from tdvutil.argparse import CheckFile
 
 state: Literal["WAITING", "STREAMING", "COOLDOWN"] = "WAITING"
 
+def log(msg: str) -> None:
+    print(msg, file=sys.stderr)
+    sys.stderr.flush()
+
+
 subproc = None
 
 def run_ffmpeg(args: argparse.Namespace):
@@ -30,7 +35,6 @@ def run_ffmpeg(args: argparse.Namespace):
 
 
     drawtext_conf = "font=mono:fontsize=48:y=h-text_h-15:box=1:boxcolor=black:boxborderw=10:fontcolor=white:expansion=normal"
-
 
     ffmpeg_cmd = [
         "ffmpeg", "-hide_banner",
@@ -45,7 +49,7 @@ def run_ffmpeg(args: argparse.Namespace):
     # ffmpeg_cmd = ["py", "./sleep.py"]
 
     try:
-        print("INFO: ffmpeg startup, output logged to /tmp/looperstream.log", file=sys.stderr)
+        log("INFO: ffmpeg startup, output logged to /tmp/looperstream.log")
         with open('/tmp/looperstream.log', "a") as logfile:
             # with open('d:/temp/looperstream.log', "a") as logfile:
             subproc = subprocess.Popen(
@@ -54,8 +58,7 @@ def run_ffmpeg(args: argparse.Namespace):
                 stderr=subprocess.STDOUT
             )
     except FileNotFoundError as e:
-        print(
-            f"ERROR: couldn't execute ffmpeg: {e}", file=sys.stderr)
+        log(f"ERROR: couldn't execute ffmpeg: {e}")
         sys.exit(1)
     # except subprocess.TimeoutExpired:
     #     print(f"ERROR: remux process timed out after {args.timeout} seconds", file=sys.stderr)
@@ -67,26 +70,26 @@ def run_ffmpeg(args: argparse.Namespace):
     #     tmpfile.unlink(missing_ok=True)
     #     sys.exit(1)
     except Exception as e:
-        print(f"ERROR: unknown error during streaming: {e}", file=sys.stderr)
+        log(f"ERROR: unknown error during streaming: {e}")
         sys.exit(1)
 
 def kill_ffmpeg():
     global subproc
 
     if subproc is None:
-        print("INFO: asked to kill ffmpeg, but it was already dead", file=sys.stderr)
+        log("INFO: asked to kill ffmpeg, but it was already dead")
         return
 
     if subproc.poll() is None:
-        print("INFO: asking ffmpeg to exit", file=sys.stderr)
+        log("INFO: asking ffmpeg to exit")
         subproc.terminate()
         subproc.wait(5)
 
     if subproc.poll() is None:
-        print("INFO: outright demanding ffmpeg to exit", file=sys.stderr)
+        log("INFO: outright demanding ffmpeg to exit")
         subproc.kill()
 
-    print("INFO: I bask in your favor, I have killed the king^W ffmpeg", file=sys.stderr)
+    log("INFO: I bask in your favor, I have killed the king^W^W ffmpeg")
     subproc = None
 
 
@@ -98,7 +101,7 @@ def check_ffmpeg():
 
     ret = subproc.poll()
     if ret is not None:  # has exited
-        print(f"WARNING: ffmpeg has exited, code {ret}", file=sys.stderr)
+        log(f"WARNING: ffmpeg has exited, code {ret}")
         subproc = None
         return False
 
@@ -106,13 +109,13 @@ def check_ffmpeg():
     return True
 
 def get_webhook_url(cfgfile: Path) -> str:
-    print(f"INFO: loading config from {cfgfile}", file=sys.stderr)
+    log(f"INFO: loading config from {cfgfile}")
     config = toml.load(cfgfile)
 
     try:
         return config["looperstream"]["webhook_url"]
     except KeyError:
-        print("ERROR: missing 'webhook_url' in config", file=sys.stderr)
+        log("ERROR: missing 'webhook_url' in config")
         sys.exit(1)
 
 # send a message to discord, of a given type, but only if the last
@@ -127,13 +130,12 @@ def send_discord(webhook_url: Optional[str], msg_type: str, msg: str) -> None:
 
     checkfile = Path(__file__).parent / "no_discord"
     if checkfile.exists():
-        print(
-            f"safe mode, not sending discord updates (to resume: rm {checkfile})", file=sys.stderr)
+        log(f"safe mode, not sending discord updates (to resume: rm {checkfile})")
         return
 
     # if msg in last_sent and now() - last_sent[msg] < 60:
     if msg == send_discord.last_sent.get(msg_type, ""):  # already sent this one!
-        print(f"DUPLICATE: {msg}", file=sys.stderr)
+        log(f"DUPLICATE: {msg}")
         return
 
     send_discord.last_sent[msg_type] = msg
@@ -141,9 +143,9 @@ def send_discord(webhook_url: Optional[str], msg_type: str, msg: str) -> None:
     webhook = DiscordWebhook(url=webhook_url, content=msg)
     response = webhook.execute()
     if response.status_code == 200:
-        print(f"SENT: {msg}", file=sys.stderr)
+        log(f"SENT: {msg}")
     else:
-        print(f"FAILED: (status {response.status_code}): {msg}", file=sys.stderr)
+        log(f"FAILED: (status {response.status_code}): {msg}")
 
     return
 
@@ -160,18 +162,18 @@ def connect_obs(host: str, port: int) -> obs.ReqClient:
     except OBSSDKRequestError as e:
         code = e.code
     except (ConnectionRefusedError, OSError):
-        # print("ERROR: Connection Refused", file=sys.stderr)
+        # log("ERROR: Connection Refused")
         return None
     except Exception as e:
-        print(f"ERROR: couldn't connect to OBS: {e}", file=sys.stderr)
+        log(f"ERROR: couldn't connect to OBS: {e}")
         return None
+
     if code != 207:
-        print(f"WARNING: Unknown OBS response code {code}", file=sys.stderr)
+        log(f"WARNING: Unknown OBS response code {code}")
         return None
 
-
-    print(f"WARNING: OBS not ready, retrying in 30 seconds", file=sys.stderr)
-    time.sleep(3)
+    log("WARNING: OBS not ready, retrying in 30 seconds")
+    time.sleep(30)
     return connect_obs(host, port)
 
 
@@ -210,7 +212,7 @@ def play_source(args: argparse.Namespace, scene: str, source: str) -> None:
         time.sleep(5)
         client.set_scene_item_enabled(scene, id, False)
     except Exception as e:
-        print(f"ERROR: couldn't play timecode source: {e}", file=sys.stderr)
+        log(f"ERROR: couldn't play timecode source: {e}")
 
 
 def parse_args() -> argparse.Namespace:
@@ -302,7 +304,7 @@ def main():
 
     atexit.register(kill_ffmpeg)
 
-    print("INFO: in startup, waiting for OBS to start streaming", file=sys.stderr)
+    log("INFO: in startup, waiting for OBS to start streaming")
     cooldown_start = 0.0
 
     while True:
@@ -311,7 +313,7 @@ def main():
 
         if state == "WAITING":
             if get_streaming(args):
-                print("INFO: OBS is streaming, starting ffmpeg", file=sys.stderr)
+                log("INFO: OBS is streaming, starting ffmpeg")
                 send_discord(webhook_url, "status", "Stream online, starting looper stream")
                 run_ffmpeg(args)
                 state = "STREAMING"
@@ -322,14 +324,14 @@ def main():
         elif state == "STREAMING":
             if not check_ffmpeg():
                 # ffmpeg has died, sigh
-                print("WARNING: ffmpeg died when we didn't expect it, restarting", file=sys.stderr)
+                log("WARNING: ffmpeg died when we didn't expect it, restarting")
                 send_discord(webhook_url, "status",
                              "Unexpected termination of looper stream (will retry)")
                 state = "WAITING"  # will restart next check
                 continue
 
             if not get_streaming(args):
-                print("INFO: OBS has stopped streaming, entering cooldown", file=sys.stderr)
+                log("INFO: OBS has stopped streaming, entering cooldown")
                 send_discord(webhook_url, "status", "Stream offline, starting cooldown")
                 cooldown_start = time.time()
                 state = "COOLDOWN"
@@ -339,7 +341,7 @@ def main():
             # Are we streaming again? If so, just go straight back into
             # streaming
             if get_streaming(args):
-                print("INFO: OBS is streaming again, continuing", file=sys.stderr)
+                log("INFO: OBS is streaming again, continuing")
                 send_discord(webhook_url, "status",
                              "Stream back online, continuing with looper stream")
                 state = "STREAMING"
@@ -347,7 +349,7 @@ def main():
 
             # Otherwise, we can be in cooldown for 5 mins before we kill ffmpeg
             if (time.time() - cooldown_start) > (5 * 60):
-                print("INFO: OBS cooldown has expired, killing ffmpeg", file=sys.stderr)
+                log("INFO: OBS cooldown has expired, killing ffmpeg")
                 send_discord(webhook_url, "status", "Cooldown ended, terminating looper stream")
                 kill_ffmpeg()
                 state = "WAITING"
--- stemstream/stemstream.py
+++ stemstream/stemstream.py
@@ -23,6 +23,11 @@ from tdvutil.argparse import CheckFile
 
 state: Literal["WAITING", "STREAMING", "COOLDOWN"] = "WAITING"
 
+def log(msg: str) -> None:
+    print(msg, file=sys.stderr)
+    sys.stderr.flush()
+
+
 subproc = None
 
 def run_ffmpeg(args: argparse.Namespace):
@@ -40,7 +45,7 @@ def run_ffmpeg(args: argparse.Namespace):
     # ffmpeg_cmd = ["py", "./sleep.py"]
 
     try:
-        print("INFO: ffmpeg startup, output logged to /tmp/stemstream.log", file=sys.stderr)
+        log("INFO: ffmpeg startup, output logged to /tmp/stemstream.log")
         with open('/tmp/stemstream.log', "a") as logfile:
             # with open('d:/temp/stemstream.log', "a") as logfile:
             subproc = subprocess.Popen(
@@ -49,8 +54,7 @@ def run_ffmpeg(args: argparse.Namespace):
                 stderr=subprocess.STDOUT
             )
     except FileNotFoundError as e:
-        print(
-            f"ERROR: couldn't execute ffmpeg: {e}", file=sys.stderr)
+        log(f"ERROR: couldn't execute ffmpeg: {e}")
         sys.exit(1)
     # except subprocess.TimeoutExpired:
     #     print(f"ERROR: remux process timed out after {args.timeout} seconds", file=sys.stderr)
@@ -62,7 +66,7 @@ def run_ffmpeg(args: argparse.Namespace):
     #     tmpfile.unlink(missing_ok=True)
     #     sys.exit(1)
     except Exception as e:
-        print(f"ERROR: unknown error during streaming: {e}", file=sys.stderr)
+        log(f"ERROR: unknown error during streaming: {e}")
         sys.exit(1)
 
 def kill_ffmpeg():
@@ -70,19 +74,19 @@ def kill_ffmpeg():
 
 
     if subproc is None:
-        print("INFO: asked to kill ffmpeg, but it was already dead", file=sys.stderr)
+        log("INFO: asked to kill ffmpeg, but it was already dead")
         return
 
     if subproc.poll() is None:
-        print("INFO: asking ffmpeg to exit", file=sys.stderr)
+        log("INFO: asking ffmpeg to exit")
         subproc.terminate()
         subproc.wait(5)
 
     if subproc.poll() is None:
-        print("INFO: outright demanding ffmpeg to exit", file=sys.stderr)
+        log("INFO: outright demanding ffmpeg to exit")
         subproc.kill()
 
-    print("INFO: I bask in your favor, I have killed the king^W ffmpeg", file=sys.stderr)
+    log("INFO: I bask in your favor, I have killed the king^W^W ffmpeg")
     subproc = None
 
 
@@ -94,7 +98,7 @@ def check_ffmpeg():
 
     ret = subproc.poll()
     if ret is not None:  # has exited
-        print(f"WARNING: ffmpeg has exited, code {ret}", file=sys.stderr)
+        log(f"WARNING: ffmpeg has exited, code {ret}")
         subproc = None
         return False
 
@@ -102,13 +106,13 @@ def check_ffmpeg():
     return True
 
 def get_webhook_url(cfgfile: Path) -> str:
-    print(f"INFO: loading config from {cfgfile}", file=sys.stderr)
+    log(f"INFO: loading config from {cfgfile}")
     config = toml.load(cfgfile)
 
     try:
         return config["stemstream"]["webhook_url"]
     except KeyError:
-        print("ERROR: missing 'webhook_url' in config", file=sys.stderr)
+        log("ERROR: missing 'webhook_url' in config")
         sys.exit(1)
 
 # send a message to discord, of a given type, but only if the last
@@ -123,13 +127,12 @@ def send_discord(webhook_url: Optional[str], msg_type: str, msg: str) -> None:
 
     checkfile = Path(__file__).parent / "no_discord"
     if checkfile.exists():
-        print(
-            f"safe mode, not sending discord updates (to resume: rm {checkfile})", file=sys.stderr)
+        log(f"safe mode, not sending discord updates (to resume: rm {checkfile})")
         return
 
     # if msg in last_sent and now() - last_sent[msg] < 60:
     if msg == send_discord.last_sent.get(msg_type, ""):  # already sent this one!
-        print(f"DUPLICATE: {msg}", file=sys.stderr)
+        log(f"DUPLICATE: {msg}")
         return
 
     send_discord.last_sent[msg_type] = msg
@@ -137,9 +140,9 @@ def send_discord(webhook_url: Optional[str], msg_type: str, msg: str) -> None:
     webhook = DiscordWebhook(url=webhook_url, content=msg)
     response = webhook.execute()
     if response.status_code == 200:
-        print(f"SENT: {msg}", file=sys.stderr)
+        log(f"SENT: {msg}")
     else:
-        print(f"FAILED: (status {response.status_code}): {msg}", file=sys.stderr)
+        log(f"FAILED: (status {response.status_code}): {msg}")
 
     return
 
@@ -156,18 +159,19 @@ def connect_obs(host: str, port: int) -> obs.ReqClient:
     except OBSSDKRequestError as e:
         code = e.code
     except (ConnectionRefusedError, OSError):
-        # print("ERROR: Connection Refused", file=sys.stderr)
+        # log("ERROR: Connection Refused"
         return None
     except Exception as e:
-        print(f"ERROR: couldn't connect to OBS: {e}", file=sys.stderr)
+        log(f"ERROR: couldn't connect to OBS: {e}")
         return None
+
     if code != 207:
-        print(f"WARNING: Unknown OBS response code {code}", file=sys.stderr)
+        log(f"WARNING: Unknown OBS response code {code}")
         return None
 
 
-    print(f"WARNING: OBS not ready, retrying in 30 seconds", file=sys.stderr)
-    time.sleep(3)
+    log(f"WARNING: OBS not ready, retrying in 30 seconds")
+    time.sleep(30)
     return connect_obs(host, port)
 
 
@@ -187,6 +191,7 @@ def get_streaming(args: argparse.Namespace) -> bool:
         connect_obs(args.host, args.port)
         r = client.get_stream_status()
     return r.output_active
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
@@ -259,16 +264,15 @@ def main():
 
     atexit.register(kill_ffmpeg)
 
-    print("INFO: in startup, waiting for OBS to start streaming", file=sys.stderr)
+    log("INFO: in startup, waiting for OBS to start streaming")
     cooldown_start = 0.0
 
     while True:
-        # print(state)
         time.sleep(5)
 
         if state == "WAITING":
             if get_streaming(args):
-                print("INFO: OBS is streaming, starting ffmpeg", file=sys.stderr)
+                log("INFO: OBS is streaming, starting ffmpeg")
                 send_discord(webhook_url, "status", "Stream online, starting stem stream")
                 run_ffmpeg(args)
                 state = "STREAMING"
@@ -276,14 +280,14 @@ def main():
         elif state == "STREAMING":
             if not check_ffmpeg():
                 # ffmpeg has died, sigh
-                print("WARNING: ffmpeg died when we didn't expect it, restarting", file=sys.stderr)
+                log("WARNING: ffmpeg died when we didn't expect it, restarting")
                 send_discord(webhook_url, "status",
                              "Unexpected termination of stem stream (will retry)")
                 state = "WAITING"  # will restart next check
                 continue
 
             if not get_streaming(args):
-                print("INFO: OBS has stopped streaming, entering cooldown", file=sys.stderr)
+                log("INFO: OBS has stopped streaming, entering cooldown")
                 send_discord(webhook_url, "status", "Stream offline, starting cooldown")
                 cooldown_start = time.time()
                 state = "COOLDOWN"
@@ -293,7 +297,7 @@ def main():
             # Are we streaming again? If so, just go straight back into
             # streaming
             if get_streaming(args):
-                print("INFO: OBS is streaming again, continuing", file=sys.stderr)
+                log("INFO: OBS is streaming again, continuing")
                 send_discord(webhook_url, "status",
                              "Stream back online, continuing with stem stream")
                 state = "STREAMING"
@@ -301,7 +305,7 @@ def main():
 
             # Otherwise, we can be in cooldown for 5 mins before we kill ffmpeg
             if (time.time() - cooldown_start) > (5 * 60):
-                print("INFO: OBS cooldown has expired, killing ffmpeg", file=sys.stderr)
+                log("INFO: OBS cooldown has expired, killing ffmpeg")
                 send_discord(webhook_url, "status", "Cooldown ended, terming stem stream")
                 kill_ffmpeg()
                 state = "WAITING"
